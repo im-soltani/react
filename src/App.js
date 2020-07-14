72d5e778eedc04da4fcb519bba5a855e2f3f8207
Imen Soltani <imensoltani6@gmail.com>
2020-07-14 13:49:08 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <form>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input type="email" class="form-control" id="inputEmail4" />
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputPassword4">Password</label>
+                <input type="password" class="form-control" id="inputPassword4" />
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="inputAddress">Address</label>
+              <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St" />
+            </div>
+            <div class="form-group">
+              <label for="inputAddress2">Address 2</label>
+              <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+            </div>
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </form>
+    </div>
+  );
+}
+
+export default App;
